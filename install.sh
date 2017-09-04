907a8f9a4e12dce2749f2c9df708fc0b52ef6245
dysoct <dysoct@gmail.com>
2017-09-04 16:12:13 +0800

    vertex-mod versioning init

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="packages/apps/Settings"
+dirs="packages/apps/Settings vendor/vertex"
 
 for dir in $dirs ; do
 	cd $rootdirectory
